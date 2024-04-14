81809ae9a6439277dc91e9cf198490bd13c0dce8
Winston <68310078+WinstonHChou@users.noreply.github.com>
2024-04-13 22:21:53 -0700

    Update joy.launch.py

--- src/launch/basestation_launch/launch/joy.launch.py
+++ src/launch/basestation_launch/launch/joy.launch.py
@@ -1,19 +1,29 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import Command
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-    joy_teleop_config = os.path.join(
-        get_package_share_directory('basestation_launch'),
+    ctrl_pkg = 'basestation_launch'
+    joy_config = os.path.join(
+        get_package_share_directory(ctrl_pkg),
         'param',
         'joy.param.yaml'
     )
-    joy_config = os.path.join(
-        get_package_share_directory('basestation_launch'),
+    joy_teleop_config = os.path.join(
+        get_package_share_directory(ctrl_pkg),
         'param',
         'joy_teleop.param.yaml'
     )
+    joy_la = DeclareLaunchArgument(
+        'joy_config',
+        default_value=joy_teleop_config,
+        description='Descriptions for joy and joy_teleop configs')
 
     ld = LaunchDescription([])
 
