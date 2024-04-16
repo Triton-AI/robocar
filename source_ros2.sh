e81b5ca925be43b967eff0b58808d8f0e398db22
jetson <jetson@ucsd-blue.local>
2024-04-15 17:19:20 -0700

    add support to rmw-cyclonedds

--- source_ros2.sh
+++ source_ros2.sh
@@ -1,6 +1,49 @@
 #!/bin/bash
 
+# Display help
+if [[ $1 == "-h" ]] || [[ $1 == "--help" ]]
+then
+  echo "Usage: source_ros2"
+  echo
+  echo "Sources the ROS distribution and workspace at WORKSPACE_DIR."
+  echo 
+  echo "containing the name of the target ROS distribution."
+  echo "Supported distros: foxy"
+  return
+fi
+
+# sudo apt update && sudo apt upgrade
+sudo apt install ros-foxy-rmw-cyclonedds-cpp
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+
 cd ~/projects/robocar
 source /opt/ros/foxy/setup.bash
 source install/setup.bash
 source src/external/lidar/install/setup.bash
+
+function source_ros2()
+{
+	cd ~/projects/robocar
+	source /opt/ros/foxy/setup.bash
+	source install/setup.bash
+	source src/external/lidar/install/setup.bash
+}
+
+function build_ros2()
+{
+	cd ~/projects/robocar
+	colcon build --packages-ignore livox_ros_driver2 livox_sdk2 --cmake-args -DCMAKE_BUILD_TYPE=Debug
+	source install/setup.bash
+}
+
+function build_ros2_pkg()
+{
+	cd ~/projects/robocar
+	if [[ $1 == "livox_ros_driver2" ]] || [[ $1 == "livox_sdk2" ]]
+	then
+  	  echo "Don't try to colcon build livox packages"
+ 	  return
+	fi
+	colcon build --packages-select $1 --cmake-args -DCMAKE_BUILD_TYPE=Debug
+	source install/setup.bash
+}
