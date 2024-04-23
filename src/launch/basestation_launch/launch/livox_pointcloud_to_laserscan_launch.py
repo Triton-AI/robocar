df3d571db9025aa925bd9589301f19483b21a91d
jetson <jetson@ucsd-blue.local>
2024-04-22 22:22:26 -0700

    add slam-toolbox support

--- src/launch/basestation_launch/launch/livox_pointcloud_to_laserscan_launch.py
+++ src/launch/basestation_launch/launch/livox_pointcloud_to_laserscan_launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
                 'angle_increment': 0.0087,  # M_PI/360.0
                 'scan_time': 0.3333,
                 'range_min': 0.0,
-                'range_max': 1.5,
+                'range_max': 15.0,
                 'use_inf': True,
                 'inf_epsilon': 1.0
             }],
