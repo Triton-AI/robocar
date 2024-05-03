dbc20fb9a50661b13c5e7ca23eac7a651f8aaf82
ucsd-blue <winston.h.chou@gmail.com>
2024-05-03 15:18:31 -0700

    create `sensors` pkg, move livox-lidar related to `sensors` pkg

--- src/launch/sensors/setup.py
+++ src/launch/sensors/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import os
 from glob import glob
 
-package_name = 'planner'
+package_name = 'sensors'
 
 setup(
     name=package_name,
@@ -14,6 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*launch.py')),
         (os.path.join('share', package_name, 'param'), glob('param/*.yaml')),
+        (os.path.join('share', package_name, 'livox_config'), glob('livox_config/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
