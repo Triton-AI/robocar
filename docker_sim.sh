b1b934912f40d8cf73f120a6ec3828ea84076069
ChenYenRu <rebecca946487@gmail.com>
2024-05-25 10:19:08 -0700

    updated docker_sim.sh

--- docker_sim.sh
+++ docker_sim.sh
@@ -11,4 +11,4 @@ docker run \
     -v /dev/bus/usb:/dev/bus/usb \
     --device /dev/video0 \
     -v  /run/user/1000/gdm/Xauthority:/root/.Xauthority:rw \
-    f1tenth_x86:latest
+    ghcr.io/triton-ai/robocar:foxy-x86
