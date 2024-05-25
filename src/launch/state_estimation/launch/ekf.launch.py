7fed695c3f3536c4275a577567262a9654b0101b
ucsd-blue <winston.h.chou@gmail.com>
2024-05-24 17:24:43 -0700

    support `r2fo_laser_odometry`

--- src/launch/state_estimation/launch/ekf.launch.py
+++ src/launch/state_estimation/launch/ekf.launch.py
@@ -6,10 +6,11 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
+import yaml
 
 def generate_launch_description():
     pkg_name = 'state_estimation'
-    config_file = 'ekf.yaml'
+    config_file = 'ekf_config.yaml'
     desc_dir = get_package_share_directory(pkg_name)
 
     config_file_path = os.path.join(
