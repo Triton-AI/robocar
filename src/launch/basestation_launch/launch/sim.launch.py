9e2fbc9896e261ecef1ac21369c270d984a4fcdf
winston_vm <winston.h.chou@gmail.com>
2024-05-30 02:23:31 +0000

    WIP: simulator devel, add `sim.launch.py` and `sim.yaml` to `basestation_launch` pkg

--- src/launch/basestation_launch/launch/sim.launch.py
+++ src/launch/basestation_launch/launch/sim.launch.py
@@ -1,25 +1,3 @@
-# MIT License
-
-# Copyright (c) 2020 Hongrui Zheng
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import Command
@@ -28,12 +6,15 @@ import os
 import yaml
 
 def generate_launch_description():
-    
+    pkg_name = 'basestation_launch'
+    desc_dir = get_package_share_directory(pkg_name)
+
     config = os.path.join(
-        get_package_share_directory('f1tenth_gym_ros'),
-        'config',
+        desc_dir,
+        'param',
         'sim.yaml'
-        )
+    )
+    
     config_dict = yaml.safe_load(open(config, 'r'))
     has_opp = config_dict['bridge']['ros__parameters']['num_agent'] > 1
     teleop = config_dict['bridge']['ros__parameters']['kb_teleop']
@@ -44,7 +25,12 @@ def generate_launch_description():
         name='bridge',
         parameters=[config]
     )
-    
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz',
+        arguments=['-d', os.path.join(get_package_share_directory('f1tenth_gym_ros'), 'launch', 'gym_bridge.rviz')]
+    )
     map_server_node = Node(
         package='nav2_map_server',
         executable='map_server',
@@ -68,22 +54,30 @@ def generate_launch_description():
         executable='robot_state_publisher',
         name='ego_robot_state_publisher',
         parameters=[{'robot_description': Command(['xacro ', os.path.join(get_package_share_directory('f1tenth_gym_ros'), 'launch', 'ego_racecar.xacro')])}],
-        remappings=[('/robot_description', 'ego_robot_description')]
+        remappings=[('/robot_description', '/ego_racecar/robot_description')]
     )
     opp_robot_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='opp_robot_state_publisher',
         parameters=[{'robot_description': Command(['xacro ', os.path.join(get_package_share_directory('f1tenth_gym_ros'), 'launch', 'opp_racecar.xacro')])}],
-        remappings=[('/robot_description', 'opp_robot_description')]
+        remappings=[('/robot_description', '/opp_racecar/robot_description')]
     )
 
     ld = LaunchDescription()
+
+    # finalize
     ld.add_action(bridge_node)
     ld.add_action(nav_lifecycle_node)
     ld.add_action(map_server_node)
-    ld.add_action(ego_robot_publisher)
+
+    if config_dict['bridge']['ros__parameters']['launch_rviz']:
+        ld.add_action(rviz_node)
+
+    if config_dict['bridge']['ros__parameters']['ego_urdf_pub']:
+        ld.add_action(ego_robot_publisher)
     if has_opp:
-        ld.add_action(opp_robot_publisher)
+        if config_dict['bridge']['ros__parameters']['opp_urdf_pub']:
+            ld.add_action(opp_robot_publisher)
 
     return ld
