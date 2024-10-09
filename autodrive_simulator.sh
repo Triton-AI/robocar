781fd8dd8c06c2563b874fc628eadad55d807bb6
chenyenru <rebecca946487@gmail.com>
2024-10-08 23:07:31 -0700

    feat: Added AutoDrive Simulator Dockerfile and entry point script
    Helpful when developing racestack with AutoDrive Simulator

---
+++ autodrive_simulator.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# AutoDRIVE Simulator Executable
+cd /home/autodrive_simulator
+
+# Launch AutoDRIVE Simulator with GUI
+# ./AutoDRIVE\ Simulator.x86_64
+
+# Launch AutoDRIVE Simulator Headless
+# ./AutoDRIVE\ Simulator.x86_64 -batchmode -nographics -ip 127.0.0.1 -port 4567
