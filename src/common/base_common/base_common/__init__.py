eb4b8443fc93ba2e40a8c046968677f03de06f6f
ucsd-blue <winston.h.chou@gmail.com>
2025-02-12 19:39:53 -0800

    added base_common

---
+++ src/common/base_common/base_common/__init__.py
@@ -0,0 +1,17 @@
+from .launch import (
+    get_share_file,
+    get_sim_time_launch_arg,
+    find_exe_preferring_local,
+    to_lower,
+    check_val_in_list,
+    get_param_file,
+)
+
+__all__ = [
+    "get_share_file",
+    "get_sim_time_launch_arg",
+    "find_exe_preferring_local",
+    "to_lower",
+    "check_val_in_list",
+    "get_param_file",
+]
---
+++ src/common/base_common/src/dummy.cpp
@@ -0,0 +1,8 @@
+// Copyright 2025 Triton-AI
+//
+// Dummy cpp
+
+namespace dummy
+{
+
+}  // namespace dummy
