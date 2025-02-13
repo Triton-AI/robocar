e5f35e5b7f5383c62920b0fb66bea5d6fddb56b2
ucsd-blue <winston.h.chou@gmail.com>
2025-02-12 22:00:34 -0800

    [Refactor] rename `vesc_odom` as `vesc_interface`, and put under control

--- src/control/vesc_interface/launch/vesc_interface.launch.py
+++ src/control/vesc_interface/launch/vesc_interface.launch.py
@@ -3,37 +3,15 @@ from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from ament_index_python.packages import get_package_share_directory
-import os
+from base_common import get_share_file
 
 def generate_launch_description():
-    actuator_pkg = 'vesc_odom'
+    launch_dir = 'vesc_interface'
 
-    ackermann_vesc_param = os.path.join(
-        get_package_share_directory("vesc_odom"),
-        'param',
-        'ackermann_vesc.param.yaml'
-    )
-    
-    vesc_odom_param = os.path.join(
-        get_package_share_directory("vesc_odom"),
-        'param',
-        'vesc_odom.param.yaml'
-    )
-
-    vesc_driver_param = os.path.join(
-        get_package_share_directory("vesc_odom"),
-        'param',
-        'vesc_driver.param.yaml'
-    )
-    
-    mux_param = os.path.join(
-        get_package_share_directory("vesc_odom"),
-        'param',
-        'ackermann_mux.param.yaml'
-    )
-
-
-    ld = LaunchDescription([])
+    ackermann_vesc_param = get_share_file(launch_dir, 'param', 'ackermann_vesc.param.yaml')
+    vesc_odom_param = get_share_file(launch_dir, 'param', 'vesc_odom.param.yaml')
+    vesc_driver_param = get_share_file(launch_dir, 'param', 'vesc_driver.param.yaml')
+    mux_param = get_share_file(launch_dir, 'param', 'ackermann_mux.param.yaml')
 
     ackermann_to_vesc_node = Node(
         package='vesc_ackermann',
@@ -60,8 +38,12 @@ def generate_launch_description():
         parameters=[mux_param],
         remappings=[('/ackermann_cmd_out', '/ackermann_cmd_output')]
     )
+
+
+    ld = LaunchDescription([])
     ld.add_action(ackermann_to_vesc_node)
     ld.add_action(vesc_to_odom_node)
     ld.add_action(vesc_driver_node)
     ld.add_action(ackermann_mux_node)
+    
     return ld
